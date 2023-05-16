3b2d2cd57c47fd6c0f4f1e20e0a9c12913c7a71d
Andre Alves <andremralves@gmail.com>
2023-05-15 21:01:00 -0300

    add new functionalities (mud and tree)
    Co-authored-by: dev-brito <britoguilherme408@gmail.com>

--- avoidTheMud-react/src/components/board.tsx
+++ avoidTheMud-react/src/components/board.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Vertex as BoardCell } from '../vertex'
 import BoardCellComponent from './boardCell'
 import { dijkstra } from '../graph'
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import { FiTarget } from 'react-icons/fi'
 
 function initBoardCells(width = 60, height = 30) {
   const rows = []
@@ -32,6 +34,7 @@ function timeout(delay: number) {
 
 const Board = () => {
   const [boardCells, setBoardCells] = React.useState(initBoardCells())
+  const [selectedObstacle, setSelectedObstacle] = React.useState('tree')
 
   function updateOneCell(boardCell: BoardCell) {
     boardCells[boardCell.position.x][boardCell.position.y] = boardCell
@@ -56,20 +59,68 @@ const Board = () => {
   return (
     <div className="flex flex-col justify-center align-middle ">
       <div>
-        <h1 className="text-4xl mb-2">Avoid the mud</h1>
-        <button
-          className="border border-sky-700 rounded mb-5 px- py-1"
-          onClick={() => startAlgorithm()}
-        >
-          Start
-        </button>
+        <h1 className="text-4xl mb-2 font-semibold">Avoid the mud</h1>
+        <div className="flex justify-between align-middle mt-10 mb-10 px-20">
+          <div className="flex my-auto">
+            <div className="w-5 h-5 mr-2">
+              <BsFillArrowRightCircleFill className="text-amber-900 w-full h-full" />
+            </div>
+            <p className="text-center">Start point</p>
+          </div>
+          <div className="flex my-auto">
+            <div className="w-5 h-5 mr-2">
+              <FiTarget className="text-amber-900 w-full h-full" />
+            </div>
+            <p>Target</p>
+          </div>
+          <button
+            className={`${
+              selectedObstacle == 'mud' ? 'bg-sky-300' : 'hover:bg-sky-300'
+            } rounded px-5 py-2 border border-sky-300`}
+            onClick={() => setSelectedObstacle('mud')}
+          >
+            <div className="flex">
+              <div className="w-5 h-5 mr-2 bg-yellow-900"></div>
+              <span>Mud</span>
+            </div>
+          </button>
+          <button
+            className={`${
+              selectedObstacle == 'tree' ? 'bg-sky-300' : 'hover:bg-sky-300'
+            } rounded px-5 py-2 border border-sky-300`}
+            onClick={() => setSelectedObstacle('tree')}
+          >
+            <div className="flex">
+              <div className="w-5 h-5 mr-2">
+                <img
+                  width="64"
+                  height="64"
+                  src="https://img.icons8.com/dusk/64/deciduous-tree.png"
+                  alt="deciduous-tree"
+                />
+              </div>
+              <span>Tree</span>
+            </div>
+          </button>
+          <button
+            className="border border-sky-700 rounded px-7 py-1"
+            onClick={() => startAlgorithm()}
+          >
+            Start
+          </button>
+        </div>
       </div>
       <table className="w-full h-3/5">
         <tbody>
           {boardCells.map((row, i) => (
             <tr key={i}>
               {row.map((cell) => (
-                <BoardCellComponent key={cell.id} boardCell={cell} />
+                <BoardCellComponent
+                  key={cell.id}
+                  boardCell={cell}
+                  updateOneCell={updateOneCell}
+                  selectedObstacle={selectedObstacle}
+                />
               ))}
             </tr>
           ))}
--- avoidTheMud-react/src/components/boardCell.tsx
+++ avoidTheMud-react/src/components/boardCell.tsx
@@ -4,9 +4,15 @@ import { FiTarget } from 'react-icons/fi'
 
 type boardCellProps = {
   boardCell: BoardCell
+  selectedObstacle: string
+  updateOneCell: (boardCell: BoardCell) => void
 }
 
-const BoardCellComponent = ({ boardCell }: boardCellProps) => {
+const BoardCellComponent = ({
+  boardCell,
+  selectedObstacle,
+  updateOneCell,
+}: boardCellProps) => {
   const icon = boardCell.start ? (
     <BsFillArrowRightCircleFill className="text-amber-900" />
   ) : boardCell.end ? (
@@ -20,14 +26,51 @@ const BoardCellComponent = ({ boardCell }: boardCellProps) => {
       ? 'bg-yellow-300'
       : boardCell.color == 'red'
       ? 'bg-red-300'
+      : boardCell.type.name == 'mud'
+      ? 'bg-yellow-900'
       : 'bg-green-300'
+
+  function addOrRemoveMud() {
+    if (boardCell.type.name != 'mud') {
+      boardCell.type = { name: 'mud', value: 3 }
+    } else {
+      boardCell.type = { name: 'grass', value: 1 }
+    }
+    updateOneCell(boardCell)
+  }
+
+  function addOrRemoveTree() {
+    if (boardCell.type.name != 'tree') {
+      boardCell.type = { name: 'tree', value: Infinity }
+    } else {
+      boardCell.type = { name: 'grass', value: 1 }
+    }
+    updateOneCell(boardCell)
+  }
+
+  function handleClick() {
+    if (boardCell.start || boardCell.end) return
+    if (selectedObstacle == 'tree') {
+      addOrRemoveTree()
+    } else {
+      addOrRemoveMud()
+    }
+  }
 
   return (
     <td
       key={boardCell.id}
       className={`border border-amber-900 w-5 h-5 ${bgColor}`}
-      onClick={() => console.log(boardCell)}
+      onClick={() => handleClick()}
     >
+      {boardCell.type.name == 'tree' && (
+        <img
+          width="64"
+          height="64"
+          src="https://img.icons8.com/dusk/64/deciduous-tree.png"
+          alt="deciduous-tree"
+        />
+      )}
       {icon}
     </td>
   )
